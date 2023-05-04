4e4bcab0380e75ff07447f8b8e7889ce20ceb38d
Arjay C. Balboa <111472877+Chopyzx@users.noreply.github.com>
2023-05-04 11:07:43 +0800

    Add files via upload

---
+++ Exercise2/Planets/Mars.java
@@ -0,0 +1,11 @@
+package Planets;
+
+import Explorers.Explorer;
+
+public class Mars implements Planet {
+    
+    public void accept(Explorer explorer) {
+        explorer.visit(this);
+    }
+}
+
